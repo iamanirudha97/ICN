525fb11366ee4a68e6aab256fb33c2c92d5bcbc9
iamnotanirudha <sagarlm10@gmail.com>
2023-03-17 19:46:12 +0530

    add a functional contact us component

--- inc-beta/src/pages/contactus/Contactus.js
+++ inc-beta/src/pages/contactus/Contactus.js
@@ -2,9 +2,55 @@ import './Contactus.css'
 
 export default function Contactus() {
   return (
-    <div>
-      <h2>Feel free to Contact us</h2>
+    <div >
+      <div className="contact-us">
+      <h2 className='contact-title'> Feel Free to Contact Us</h2>
+      <iframe 
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15024.618482090194!2d72.7835079!3d19.7060402!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be71c92fbd91e67%3A0x2a3ce68676417a45!2sSt.%20John%20College%20of%20Engineering%20and%20Management!5e0!3m2!1sen!2sin!4v1679058180370!5m2!1sen!2sin" 
+        height="450" 
+        style= {{border:0, width : '100%', marginTop: '40px', boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px'}}
+        allowFullScreen="" 
+        loading="lazy" 
+        referrerpolicy="no-referrer-when-downgrade">
+      </iframe>
+
+      </div>
       
+
+      <div class="container">
+        <form action="https://formspree.io/f/xbjekvvo" method="POST" className='contact-us-form'>
+          <input 
+            type="text"
+            name="username"
+            placeholder="username"
+            autoComplete="off"
+            required
+          />
+
+          <input 
+            type="email"
+            name="Email"
+            placeholder="enter your email ID"
+            autoComplete="off"
+            required
+          />
+
+          <textarea 
+            name="message" 
+            cols="30" 
+            rows="6" 
+            autoComplete='off'
+            required
+            placeholder='Your message'
+          ></textarea>
+
+          <input type="submit" value="SEND" className='form-btn' />
+        
+      </form>
+    </div>
+
     </div>
   )
 }
+
+
